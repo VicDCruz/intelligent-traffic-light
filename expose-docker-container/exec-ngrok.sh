c7d01f2fb369881ebdf52ebe882a1077316e5948
Victor Cruz <vicdan215@gmail.com>
2020-05-18 15:06:30 -0500

    Add ngrok

---
+++ expose-docker-container/exec-ngrok.sh
@@ -0,0 +1,3 @@
+echo Received docker container: $1
+
+docker run -d -p 4040:4040 –-network=intelligent-traffic-light_default –-name ngrok wernight/ngrok ngrok http $1:80
---
+++ expose-docker-container/install-ngrok.sh
@@ -0,0 +1,1 @@
+docker pull wernight/ngrok
